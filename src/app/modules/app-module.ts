fd36368262d78b7eec540841dc00aa225c2b2595
Brett Boettcher <hgdxg@BrettsRTX>
2025-08-20 17:46:48 -0600

    completed unit #3

--- src/app/modules/app-module.ts
+++ src/app/modules/app-module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing-module';
 import { App } from '../app';
 
+import { Analytics } from '../components/analytics/analytics';
+import { AnalyticsTable } from '../components/analytics-table/analytics-table';
+import { Departments } from '../components/departments/departments';
+import { Timesheet } from '../components/timesheet/timesheet';
+import { TopNavbar } from '../components/top-navbar/top-navbar';
+import { MaterialModule } from './material-module';
+
 @NgModule({
-  declarations: [App],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [App, Departments, Analytics, AnalyticsTable, Timesheet, TopNavbar],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
+
 export class AppModule {}
