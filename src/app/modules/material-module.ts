ce591ca8f869624c627c0f74b65513ad3ef33056
Brett Boettcher <hgdxg@BrettsRTX>
2025-08-29 16:26:45 -0600

    fixed button positioning on the timesheet form

--- src/app/modules/material-module.ts
+++ src/app/modules/material-module.ts
@@ -5,7 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { MatError, MatInput } from '@angular/material/input';
+import { MatError, MatInput, MatLabel } from '@angular/material/input';
 import { MatFormField } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -22,6 +22,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormField,
     ReactiveFormsModule,
     MatError,
+    MatLabel,
   ],
   exports: [
     MatToolbarModule,
@@ -33,6 +34,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormField,
     ReactiveFormsModule,
     MatError,
+    MatLabel,
   ],
 })
 export class MaterialModule {}
